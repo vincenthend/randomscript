3e63ff6ec5a8e19418c11fe4efcf274f1dab1f85
Vincent Hendryanto H <vincenthendrha@gmail.com>
2018-02-27 15:21:44 +0700

    Update summonclock.js

--- PTIClock/summonclock.js
+++ PTIClock/summonclock.js
@@ -46,4 +46,30 @@ function styleClock(element){
 	element.childNodes[1].appendChild(textarea);
 }
 
+function addCountdown(endTime){	
+	var timeBlock = document.getElementById("block_progress_serverTime");
+
+	var servTime = timeBlock.value.split(":");
+	var endTime = endTime.split(":");
+	
+	var now = parseInt(servTime[0]) * 3600 + parseInt(servTime[1]) * 60 + parseInt(servTime[2]);
+	var end = parseInt(endTime[0]) * 3600 + parseInt(endTime[1]) * 60 + parseInt(endTime[2]);
+	
+	var timeLeft = end - now;
+	if(timeLeft > 0){
+		var timeLeftBlock = document.createElement("div");
+		timeLeftBlock.id = "timeLeft";
+		timeBlock.parentNode.appendChild(timeLeftBlock);
+		setInterval(function(){updateCountdown(timeLeft);timeLeft--;},1000);
+	}	
+}
+
+function updateCountdown(timeLeft){
+	var left = document.getElementById("timeLeft");
+	var hrsLeft = Math.floor(timeLeft/3600);
+	var minLeft = Math.floor((timeLeft%3600)/60);
+	var secLeft = timeLeft%60;
+	left.innerHTML = hrsLeft +" hours " + minLeft + " minutes " + secLeft + " seconds ";
+}
+
 showClockBlock()
